6d5d67ec5acfaf05284ff63c65288636886261bf
jesuszdp <jesusz.unam@gmail.com>
2022-10-18 20:25:21 -0500

    Sesión 1. E2

---
+++ s1/e2.js
@@ -0,0 +1,25 @@
+//npm i moment
+
+//
+/*
+Para eliminar una librería
+Paso 1. Eliminar la carpeta node_modules
+Paso 2. Ejecutar
+    npm i
+
+Para instalar otra versión
+Paso 1. Eliminar la carpeta node_modules
+Paso 2. Modificar package.json la versión
+Paso 3. ejecutar:
+    npm i
+*/
+let os = require('os');
+let misCpu = os.cpus();
+console.log(misCpu);
+
+
+/*
+let moment = require('moment');
+let now = moment();
+console.log(now);
+*/
